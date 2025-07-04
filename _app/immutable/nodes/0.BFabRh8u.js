783cf5b0b40e9e04b37a3b2868ae2ba07ff59656
alexyvanot <48055002+alexyvanot@users.noreply.github.com>
2025-07-04 13:30:35 +0000

    Deploying to gh-pages from @ alexyvanot/blog@e8ffaa4d5290bb98cf86d7c14250047420909864 🚀

---
+++ _app/immutable/nodes/0.BFabRh8u.js
@@ -0,0 +1,1 @@
+import"../chunks/CWj6FrbW.js";import{C as a,D as n,F as s}from"../chunks/BRSlwceG.js";import{s as i}from"../chunks/Bk5eIVZx.js";const l=!0,p="always",d=Object.freeze(Object.defineProperty({__proto__:null,prerender:l,trailingSlash:p},Symbol.toStringTag,{value:"Module"}));function f(t,o){var e=a(),r=n(e);i(r,()=>o.children),s(t,e)}export{f as component,d as universal};
