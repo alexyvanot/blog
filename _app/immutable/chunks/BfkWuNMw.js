e5d85411e78f5c1ba29ccaa29d35b07a7547aa51
alexyvanot <48055002+alexyvanot@users.noreply.github.com>
2025-07-15 15:26:10 +0000

    Deploying to gh-pages from @ alexyvanot/testing-blog@063e308f019ea3600da3b46625ce281ab6b51fda 🚀

---
+++ _app/immutable/chunks/BfkWuNMw.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1xow9a4)==null?void 0:s.base)??"/testing-blog";var a;const e=((a=globalThis.__sveltekit_1xow9a4)==null?void 0:a.assets)??t;export{e as a,t as b};
